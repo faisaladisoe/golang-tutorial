f7c29c963d2f36bda828bd4f43005d44935bce06
Muhammad Faisal Adi Soesatyo <faisaladi27@gmail.com>
2023-08-18 19:01:37 +0700

    continuing the creating module and executable file

--- learn-golang-module/greetings/greetings.go
+++ learn-golang-module/greetings/greetings.go
@@ -1,8 +1,54 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	greet := fmt.Sprintf("Hi, %s. Welcome!", name)
-	return greet
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+
+	// Create a message using a random format.
+	greet := fmt.Sprintf(randomFormat(), name)
+	// greet := fmt.Sprint(randomFormat()) 		// check if the test work
+	return greet, nil
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+    // A map to associate names with messages.
+    messages := make(map[string]string)
+    // Loop through the received slice of names, calling
+    // the Hello function to get a message for each name.
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        // In the map, associate the retrieved message with
+        // the name.
+        messages[name] = message
+    }
+    return messages, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
 }
--- learn-golang-module/hello/hello.go
+++ learn-golang-module/hello/hello.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"example/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("Crystal")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+	// A slice of names.
+    names := []string{"Crystal", "Abraham", "Jessica"}
+
+	// Request a greeting message.
+    messages, err := greetings.Hellos(names)
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	// If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
